32cd38521f82f1825eff5485c0a1a7829f12e9a7
Henard Gertzen <henardgertzen@gmail.com>
2024-06-03 04:46:59 +0200

    Fixed credentials bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,5 @@ import Home from './Home';
 import About from './About';
 import Projects from './Projects';
 import Contact from './Contact';
-import Credentials from './Credentials';
 
-export { Home, About, Projects, Contact, Credentials };
+export { Home, About, Projects, Contact};
