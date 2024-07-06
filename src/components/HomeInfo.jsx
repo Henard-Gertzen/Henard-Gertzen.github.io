c135aa971d6ed3c8e6648c8f147287af2c94b421
Henard Gertzen <henardgertzen@gmail.com>
2024-07-06 21:08:07 +0200

    Added CV

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -5,15 +5,30 @@ import { arrow } from "../assets/icons";
 const HomeInfo = ({ currentStage }) => {
   if (currentStage === 1)
     return (
-      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-        Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Henard</span>
-        <br />
-        A Software Developer from South-Africa! 🇿🇦
-      </h1>
+        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+          Hi, I'm
+          <span className='font-semibold mx-2 text-white'>Henard</span>
+          <br />
+          A Software Developer from South-Africa! 🇿🇦
+        </h1>
     );
 
-  if (currentStage === 2) {
+    if (currentStage === 2) {
+      return (
+        <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+          Need a project done or looking for a dev? <br/> I am just a click away
+        </p>
+  
+        <Link to='https://www.linkedin.com/in/henard-gertzen-5b4267199/' className='neo-brutalism-white neo-btn'>
+          Get in Contact!
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+      );
+    }
+
+  if (currentStage === 3) {
     return (
       <div className='info-box'>
         <p className='font-medium sm:text-xl text-center'>
@@ -28,7 +43,7 @@ const HomeInfo = ({ currentStage }) => {
     );
   }
 
-  if (currentStage === 3) {
+  if (currentStage === 4) {
     return (
       <div className='info-box'>
         <p className='font-medium text-center sm:text-xl'>
@@ -43,20 +58,6 @@ const HomeInfo = ({ currentStage }) => {
     );
   }
 
-  if (currentStage === 4) {
-    return (
-      <div className='info-box'>
-      <p className='font-medium sm:text-xl text-center'>
-        Need a project done or looking for a dev? <br/> I am just a click away
-      </p>
-
-      <Link to='/contact' className='neo-brutalism-white neo-btn'>
-        Get in Contact!
-        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
-      </Link>
-    </div>
-    );
-  }
 
   return null;
 };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Loader } from '@react-three/drei'
+import React, { Suspense } from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <Suspense fallback = { <Loader />}>
+    <section className="relative flex lg:flex-row flex-col
+    max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">
+          My CV
+        </h1>
+        <embed src="Resume-2024.pdf" className='h-screen' type="application/pdf"/>
+      </div>
+    </section>
+    </Suspense>
   )
 }
 
